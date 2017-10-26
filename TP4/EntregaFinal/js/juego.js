096ff068809cdbaca9ec45a800d4cc30f082750e
Emmanuel Flores <emmanuelflores2010@gmail.com>
2017-10-25 23:45:50 -0300

    esto es lo que llegue a hacer

---
+++ TP4/EntregaFinal/js/juego.js
@@ -0,0 +1,7 @@
+$(document).keydown(function(e){
+  if (e.keyCode === 39) {
+    $(".background_1").addClass("animacion_bg1");
+    $(".background_2").addClass("animacion_bg2");
+    $(".background_3").addClass("animacion_bg3");
+  }
+})

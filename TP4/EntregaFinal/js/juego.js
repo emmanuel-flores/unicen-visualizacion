a5294dc9f7f05e899d05d85f21c2ae8b95c2a51c
Emmanuel Flores <emmanuelflores2010@gmail.com>
2017-10-27 05:54:05 -0300

    Agrego personaje con animaciones

--- TP4/EntregaFinal/js/juego.js
+++ TP4/EntregaFinal/js/juego.js
@@ -1,7 +1,41 @@
-$(document).keydown(function(e){
-  if (e.keyCode === 39) {
-    $(".background_1").addClass("animacion_bg1");
-    $(".background_2").addClass("animacion_bg2");
-    $(".background_3").addClass("animacion_bg3");
+var personaje = document.getElementById('pj');
+var background_1 = document.getElementById('bg1');
+var background_2 = document.getElementById('bg2');
+var background_3 = document.getElementById('bg3');
+const flechaDer = 39;
+const flechaUp = 38;
+const flechaIzq = 37;
+const flechaDown = 40;
+
+function correr(e){
+  if (e.keyCode === flechaDer) {
+    background_1.classList.add('animacion_bg1');
+    background_2.classList.add('animacion_bg2');
+    background_3.classList.add('animacion_bg3');
+    personaje.classList.remove('quieto');
+    personaje.classList.add('correr');
+  }else if (e.keyCode === flechaUp && personaje.classList.contains('correr')) {
+    personaje.classList.remove('correr');
+    personaje.classList.add('saltar');
+    personaje.addEventListener("animationend", function() {
+      personaje.classList.remove('saltar');
+      personaje.classList.add('correr');
+    })
+  }else if (e.keyCode === flechaIzq && personaje.classList.contains('correr')) {
+    personaje.classList.remove('correr');
+    personaje.classList.add('ataque');
+    personaje.addEventListener("animationend", function() {
+      personaje.classList.remove('ataque');
+      personaje.classList.add('correr');
+    })
+  }else if (e.keyCode === flechaDown && personaje.classList.contains('correr')) {
+    personaje.classList.remove('correr');
+    personaje.classList.add('rodar');
+    personaje.addEventListener("animationend", function() {
+      personaje.classList.remove('rodar');
+      personaje.classList.add('correr');
+    })
   }
-})
+}
+
+document.addEventListener("keydown", correr);
